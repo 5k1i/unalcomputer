11290846e2f999721ca32fa5e03c921fe400bd00
Salahi Halil Altinci <salahialtinci@gmail.com>
2024-04-27 21:52:05 +0300

    Revert #3 ve #4

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,22 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $admin = User::factory()->create([
-            'name' => 'Test Admin',
-            'password' => bcrypt('password'),
-            'email' => 'test@example.com',
-        ]);
 
-        $user = User::factory()->create([
-            'name' => 'Test User',
-            'password' => bcrypt('password'),
-            'email' => 'user@example.com',
-        ]);
-
-        $adminRole = Role::create(['name' => 'Admin']);
-        $userRole = Role::create(['name' => 'User']);
-
-        $admin->assignRole($adminRole);
-        $user->assignRole($userRole);
     }
 }
